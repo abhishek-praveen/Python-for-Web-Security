9299019acd98a0dad722e8d6880def2d083d8a27
Abhiiitan <abhishekpraveen@am.students.amrita.edu>
2024-10-27 22:34:50 +0530

    Minor fixes

--- bannergrabber.py
+++ bannergrabber.py
@@ -6,14 +6,19 @@ def banner(ip,port):
         s = socket.socket()
         s.connect((ip,int(port)))
         s.settimeout(5)
-        print(str(s.recv(1024)).strip('b'))
+        print((s.recv(1024)).decode('utf-8').strip())
+        s.close()
     except KeyboardInterrupt:
         print("\n Exiting...")
         sys.exit()
+    except ValueError:
+        print("\nInvalid Port number. Enter port number as integer. ")
     except ConnectionRefusedError:
         print("\n Connection refused...")
     except TimeoutError:
         print("\n Connection timed out..." )
+    except Exception as e:
+        print(f"\nAn error occurred:{e}")
 
 
 
